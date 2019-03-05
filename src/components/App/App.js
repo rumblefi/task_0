6558151e2120708395e1f1d1d2edce0407e0507a
Yuriy Bahur <yuriybahur1@gmail.com>
2019-03-05 22:46:34 +0200

    Not Found

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,11 +2,12 @@ import React, {Component} from 'react'
 import 'normalize.css'
 import './App.scss'
 import Header from '../Header/Header';
-import {BrowserRouter as Router, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import Home from '../pages/Home/Home'
 import News from '../pages/News/News'
 import Login from '../pages/Login/Login'
 import Profile from '../Profile/Profile'
+import NotFound from '../NotFound/NotFound'
 
 export default class App extends Component{
 
@@ -35,14 +36,17 @@ export default class App extends Component{
 				<div className="app">
 					<Header />
 						<div className="app__content">
-							<Route path="/" exact component={Home} />
-							<Route path="/news" component={News} />
-							<Route path="/profile" component={ () =>  (
-								<Profile isLoggedIn={isLoggedIn} onLogOut={this.onLogOut} />
-							)} />
-							<Route path="/login" component={ () => (
-								<Login onLogIn={this.onLogIn} isLoggedIn={isLoggedIn} />
-							)} />
+							<Switch>
+								<Route path="/" exact component={Home} />
+								<Route path="/news" component={News} />
+								<Route path="/profile" component={ () =>  (
+									<Profile isLoggedIn={isLoggedIn} onLogOut={this.onLogOut} />
+								)} />
+								<Route path="/login" component={ () => (
+									<Login onLogIn={this.onLogIn} isLoggedIn={isLoggedIn} />
+								)} />
+								<Route component={NotFound} />
+							</Switch>	
 						</div>
 				</div>
 			</Router>
---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './NotFound.scss'
+
+const NotFound = () => {
+	return(
+		<div className="not-found">Not Found!</div>
+	)
+}
+
+export default NotFound
